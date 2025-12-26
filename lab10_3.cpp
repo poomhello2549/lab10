fbbc5df8c18d31f0c782ec1d844536c974ac27e8
poomhello2549 <poomhello2549@gmail.com>
2025-12-26 20:21:01 +0700

    Update lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,31 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    ifstream source("score.txt");
+    double num, sum = 0, sum_sq = 0;
+    int count = 0;
+
+    while (source >> num) {
+        sum += num;           
+        sum_sq += num * num;  
+        count++;              
+    }
+
+    
+    double mean = sum / count;
+    double std_dev = sqrt((sum_sq / count) - pow(mean, 2));
+
+    
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << std_dev << endl;
+
+    source.close();
+    return 0;
+}
